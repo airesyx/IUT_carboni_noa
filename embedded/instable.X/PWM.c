3741f2fb5d5d40dbe0345600ddfee84e5b79fed9
iutgeiitoulonE105 <touloniut@gmail.com>
2026-09-09 16:05:16 +0200

    DVVVV DVVVV DVVVV DVVVVVVVVV

--- embedded/instable.X/PWM.c
+++ embedded/instable.X/PWM.c
@@ -1,6 +1,7 @@
 #include <xc.h>
 #include "IO.h"
 #include "PWM.h"
+#include "Toolbox.h"
 
 #define PWMPER 24.0
 
@@ -8,16 +9,16 @@ void InitPWM(void) {
     PTCON2bits.PCLKDIV = 0b000; //Divide by 1
     PTPER = 100 * PWMPER;
 
-    //Réglage PWM moteur 1 sur hacheur 1
+    //Reglage PWM moteur 1 sur hacheur 1
     IOCON1bits.PMOD = 0b11; //PWM I/O pin pair is in the True Independent Output mode
     IOCON1bits.PENL = 1;
     IOCON1bits.PENH = 1;
-    FCLCON1 = 0x0003; //Désactive la gestion de faults
+    FCLCON1 = 0x0003; //Desactive la gestion de faults
 
     IOCON2bits.PMOD = 0b11; //PWM I/O pin pair is in the True Independent Output mode
     IOCON2bits.PENL = 1;
     IOCON2bits.PENH = 1;
-    FCLCON2 = 0x0003; //Désactive la gestion des faults
+    FCLCON2 = 0x0003; //Desactive la gestion des faults
     
     /*Enable PWM Module*/
     PTCONbits.PTEN = 1;
@@ -38,12 +39,12 @@ void PWMSetSpeed(float vitesseEnPourcents, uint8_t selMotor){
     }
     else if (selMotor == MOTEUR_DROIT){
         if(vitesseEnPourcents >= 0){
-            PDC2 = vitesseEnPourcents * PWMPER + talon;
-            SDC2 = talon; 
+            PDC2 = talon;
+            SDC2 = vitesseEnPourcents * PWMPER + talon;
         }
         else {
-            PDC2 = talon;
-            SDC2 = -vitesseEnPourcents * PWMPER + talon;
+            PDC2 = -vitesseEnPourcents * PWMPER + talon;
+            SDC2 = talon; 
         }
     }
 }
